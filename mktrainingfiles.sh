6b7b2191d30c6ed0279b998f8669b67fa4e457b1
reckbo <reckbo@bwh.harvard.edu>
2015-10-09 01:02:04 -0400

    ENH: make mktrainingfiles.sh accept directory argument

--- mktrainingfiles.sh
+++ mktrainingfiles.sh
@@ -4,11 +4,21 @@ SCRIPT=$(readlink -m $(type -p "$0"))
 SCRIPTDIR=${SCRIPT%/*}      
 
 usage() {
-    echo -e "Makes 'masks.txt' and 't1s.txt'"
+    echo -e "Makes 'masks.txt', 't1s.txt' and 'trainingdata.csv'
+Usage: 
+    ${1##*/} [<dir>]
+
+    <dir>   output directory (default: current directory)
+    "
 }
 
-[ $# -eq 0 ] || usage
+dirOutput="."
+if [ $# -gt 0 ]; then 
+    [[ "$1" != "-h" ]] || { usage; exit 0; }
+    dirOutput=$1
+fi
 
 datadir="$SCRIPTDIR/"
-ls -1 $datadir/*edited.nrrd | sed "s|.*\/|$datadir|" > masks.txt
-ls -1 $datadir/*realign.nrrd | sed "s|.*\/|$datadir|" > t1s.txt
+ls -1 $datadir/*edited.nrrd | sed "s|.*\/|$datadir|" > $dirOutput/masks.txt
+ls -1 $datadir/*realign.nrrd | sed "s|.*\/|$datadir|" > $dirOutput/t1s.txt
+paste -d, $dirOutput/t1s.txt $dirOutput/masks.txt > $dirOutput/trainingDataT1.csv
